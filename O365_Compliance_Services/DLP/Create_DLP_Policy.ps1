3b35323c1953936f191f1404138d79476d4559fd
Wesley Blackwell <40415351+Leafry@users.noreply.github.com>
2023-09-24 21:48:21 -0400

    udpate

---
+++ O365_Compliance_Services/DLP/Create_DLP_Policy.ps1
@@ -0,0 +1,60 @@
+﻿<#
+
+    -Created by: Wesley Blackwell
+    -Date last updated: 9/24/2023
+
+    -Overview:
+        This script contains several commands for DLP policy and rule creation.
+
+    -Permissions Needed:
+        -Global Admin
+        -Compliance Admin
+
+    -Modules Needed:
+        -ExchangeOnlineManagement
+
+    -Notes:
+        -Cmdlets will be downloaded when the session is active. 
+        -Security permissions need to be active first. If user is not an admin, the command will just fail to execute without giving a permission error.
+        -Can take up to 15 minutes for policy to sync
+        -Sensitive labels need the id GUID in place of the name, this can also cause issues in the portal where the name shows blank
+        -IncidentReport send alert to to admins does not seem to work. GenerateAlert and GenerateIncidentReport does not activate it.
+        -Value "-1"  is "Any" for sensitive information type, within the .txt file for rule
+#>
+
+Import-Module ExchangeOnlineManagement
+
+Connect-IPPSSession -UserPrincipalName "user@contoso.com"
+
+#Get a sensitive information type by name
+Get-DlpSensitiveInformationType -Identity "My Policy Name" | Format-List
+
+#Get a list of all DLP Policies
+Get-DlpCompliancePolicy
+
+#Get a specific policy by name
+Get-DlpCompliancePolicy -Identity "My Policy Name" | Format-List
+
+#Get a specific rule by name
+Get-DlpComplianceRule -Identity "My Rule Name" | Format-List
+#Same as above but output to file
+Get-DlpComplianceRule -Identity "My Rule Name"| Format-List | Out-File -FilePath "C:\TEMP\DLP_Policy\output.txt"
+
+#Create new DLP policy
+New-DlpCompliancePolicy -Name "My Policy Name" -ExchangeLocation All -Mode TestWithoutNotifications
+
+#Read the content of a .txt file to create a new rule
+$rule = Get-Content -Path "C:\TEMP\DLP_Policy\rule.txt" -ReadCount 0
+#Read the content of a .txt file to create a new custom email body
+$email = Get-Content -Path "C:\TEMP\DLP_Policy\email.txt" -ReadCount 0
+
+#Create new DLP rule
+New-DLPComplianceRule -Name "My Rule Name" -Policy "My Policy Name" `
+-AdvancedRule $rule -NotifyUser LastModifier, user@contoso.com, user2@contoso.com `
+-NotifyUserType Email -NotifyEmailCustomText $email -Priority 0
+
+#Set new values for a DLP rule
+Set-DLPComplianceRule -Identity "My Rule Name" -NotifyUser LastModifier, user@contoso.com, user2@contoso.com `
+-NotifyUserType Email -NotifyEmailCustomText $email -GenerateAlert SiteAdmin -Priority 0
+
+Set-DLPComplianceRule -Identity "My Rule Name" -GenerateIncidentReport "user@contoso.com"
--- O365_Compliance_Services/MIP/README.md
+++ O365_Compliance_Services/MIP/README.md
@@ -1,15 +1,24 @@
-<h1>Overview of Folders</h1>
-<h2>Administrative Tasks</h2>
-• Scripts</br>
-<h2>Best Practices</h2>
-• Best practices of course</br>
-<h2>Configuration</h2>
-<h3>Contains:</h3>
-• Scripts</br>
-• FAQs </br>
-<h2>Troubleshooting</h2>
-<h3>Contains:</h3>
-• Scripts</br>
-• Additional Troubleshooting Tools</br>
-• Other Troubleshooting Tips</br>
-• MSFT Troubleshooting Docs</br>
+<h1>Troubleshooting Scripts</h1>
+<h3>1. Export_Labels.ps1</h3>
+&emsp;• Pull down all labels and label policies in an enviornment and export it to a txt file.<br>
+<h3>2. Export_IRM_configuration.ps1</h3>
+&emsp;• Display what the active IRM settings are. This can be useful in troubleshooting MIP.<br>
+<h3>3. Retry_Policy_Distribution.ps1</h3>
+&emsp;• Retry policy distribution if the distribution status may be hung up (i.e. pending).<br>
+<h3>4. Export_AIP_Service_Admin_Log.ps1</h3>
+&emsp;• Export any admin commands that have been ran, including PowerShell commands.<br>
+<h3>5. Export_AIP_Service_Information.ps1</h3>
+&emsp;• Export the AIP service information such as if it is on, what platforms is it turned on for, etc...<br>
+<br>
+<br>
+<h1>Additional Tools</h1>
+<h3>1. Unified Labeling Support Tool: https://github.com/microsoft/UnifiedLabelingSupportTool</h3>
+    &emsp;• The 'Unified Labeling Support Tool' provides the functionality to reset all corresponding client services (UL, AIP, MIP, etc.). Its main purpose is to delete the currently downloaded sensitivity label policies and thus reset all settings, and it can also be used to collect data for failure analysis and problem solving.<br>
+<br>
+<br>
+<h1>Other Tips</h1>
+<h3>1. Labels do not work at all</h3>
+    &emsp;• If labels do not work in your environment at all, there could be a severe issue. I have noticed this issue along with other issues such as Safe Links breaking as well. A potential fix for this is a full tenant sync. This is not a procedure that regular org admins can make. This procedure is performed by Microsoft customer support.<br>
+<h1>MSFT TroubleShooting Docs</h1>
+    &emsp;• <a href="https://support.microsoft.com/en-us/office/known-issues-with-sensitivity-labels-in-office-b169d687-2bbd-4e21-a440-7da1b2743edc#ID0EDD=Office_365">Known issues with sensitivity labels in Office.</a><br>
+    &emsp;• <a href="https://docs.microsoft.com/en-us/troubleshoot/azure/general/troubleshoot-aip-issues">How to troubleshoot Azure Information Protection policy issues</a><br>
