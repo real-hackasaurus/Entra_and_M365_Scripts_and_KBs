bb2a7bf5c8d6577c70a0595dd8630adf6d82ab79
Wes <wesleyblackwell1@gmail.com>
2023-10-17 14:26:47 -0400

    update group script

--- Azure_Active_Directory/Groups/Remove_Member_AAD.ps1
+++ Azure_Active_Directory/Groups/Remove_Member_AAD.ps1
@@ -1,37 +1,29 @@
 <#
 .SYNOPSIS
-This script removes specified users from an Azure Active Directory (AAD) group.
+This script removes specified users from multiple Azure Active Directory (AAD) groups.
 
 .DESCRIPTION
-This script connects to Azure AD using the AzureAD module and removes the provided users from a specified AAD group. 
-The users to be removed are passed as a comma-separated string.
-The group and users are specified as command line parameters.
+This script connects to Azure AD using the AzureAD module and removes the provided users from the specified AAD groups. 
+The users to be removed and groups are passed as comma-separated strings.
+Both groups and users are specified as command line parameters.
 
-.PARAMETER groupName
-The name of the Azure Active Directory group from which users will be removed.
+.PARAMETER groupNames
+A comma-separated string of Azure Active Directory group names from which users will be removed.
 
 .PARAMETER users
-A comma-separated string of user principal names to be removed from the specified group.
+A comma-separated string of user principal names to be removed from the specified groups.
 
 .EXAMPLE
-.\Remove_Member_AAD.ps1 -groupName "MyTestGroup" -users "user1@example.com,user2@example.com,user3@example.com"
+.\Remove_Member_AAD.ps1 -groupNames "Group1,Group2" -users "user1@example.com,user2@example.com"
 
-This will attempt to remove the users user1@example.com, user2@example.com, and user3@example.com from the group named "MyTestGroup" in Azure AD.
+This will attempt to remove the users user1@example.com and user2@example.com from the groups named "Group1" and "Group2" in Azure AD.
 
 #>
-
-# Ensure AzureAD module is installed
-if (-not (Get-Module -ListAvailable -Name AzureAD)) {
-    Install-Module -Name AzureAD -Confirm:$false -Force
-}
-
-# Import the AzureAD module
-Import-Module AzureAD
 
 # Get command line parameters
 param (
     [Parameter(Mandatory=$true)]
-    [string]$groupName,
+    [string]$groupNames,
 
     [Parameter(Mandatory=$true)]
     [string]$users
@@ -40,26 +32,29 @@ param (
 # Connect to Azure AD
 Connect-AzureAD
 
-# Get the group based on the group name
-$group = Get-AzureADGroup -Filter "DisplayName eq '$groupName'"
+# Split the groupNames string into individual group names
+$groupNames.Split(',').Trim() | ForEach-Object {
+    $groupName = $_
+    $group = Get-AzureADGroup -Filter "DisplayName eq '$groupName'"
 
-if ($null -eq $group) {
-    Write-Error "Group '$groupName' not found."
-    exit 1
-}
+    if ($null -eq $group) {
+        Write-Error "Group '$groupName' not found."
+        continue
+    }
 
-# Split the user string into individual usernames and remove them from the group
-$users.Split(',').Trim() | ForEach-Object {
-    $user = Get-AzureADUser -Filter "UserPrincipalName eq '$_'"
-    
-    if ($null -eq $user) {
-        Write-Warning "User '$_' not found."
-    } else {
-        try {
-            Remove-AzureADGroupMember -ObjectId $group.ObjectId -MemberId $user.ObjectId
-            Write-Output "Removed user '$_' from group '$groupName'."
-        } catch {
-            Write-Error "Failed to remove user '$_' from group '$groupName'. Error: $_"
+    # Split the user string into individual usernames and remove them from the group
+    $users.Split(',').Trim() | ForEach-Object {
+        $user = Get-AzureADUser -Filter "mail eq '$_'"
+
+        if ($null -eq $user) {
+            Write-Warning "User '$_' not found."
+        } else {
+            try {
+                Remove-AzureADGroupMember -ObjectId $group.ObjectId -MemberId $user.ObjectId
+                Write-Output "Removed user '$_' from group '$groupName'."
+            } catch {
+                Write-Error "Failed to remove user '$_' from group '$groupName'. Error: $_"
+            }
         }
     }
 }
