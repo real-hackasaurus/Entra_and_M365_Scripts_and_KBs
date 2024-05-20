1ae95bf2baffd2f5222edc03928344c21ba1f4c0
Wes <wesleyblackwell1@gmail.com>
2024-05-19 21:10:13 -0400

    update

--- O365_Compliance_Services/Audit_Log/Common_Searches/SPO_Site_Deleted.ps1
+++ O365_Compliance_Services/Audit_Log/Common_Searches/SPO_Site_Deleted.ps1
@@ -9,6 +9,39 @@
     #-take parameters
     #-search audit log
     #-export result to csv
+
+<#
+.SYNOPSIS
+This script searches the Microsoft 365 Purview Audit Log for the deletion of a specified SharePoint site and exports the results to a CSV file.
+
+.DESCRIPTION
+This PowerShell script connects to Microsoft 365 Compliance Center, searches the audit log for events where a specified SharePoint site has been deleted within a given date range, and exports the findings to a CSV file. The user can specify a precise start and end date or a number of days back from the current date to search.
+
+.PARAMETERS
+- SiteUrl [Mandatory]: The URL of the SharePoint site to check for deletion events.
+- StartDate [Optional]: The start date for the search range. Must be used with EndDate.
+- EndDate [Optional]: The end date for the search range. Must be used with StartDate.
+- DaysBack [Optional]: The number of days to search back from today's date. Cannot be used with StartDate or EndDate.
+
+.EXAMPLE
+PS> .\SPO_Site_Deleted.ps1 -SiteUrl "https://example.sharepoint.com/sites/sitename" -StartDate "2023-05-01" -EndDate "2023-05-17"
+
+This example searches for deletion events for the specified SharePoint site between May 1, 2023, and May 17, 2023, and exports the results to a CSV file.
+
+.EXAMPLE
+PS> .\SPO_Site_Deleted.ps1 -SiteUrl "https://example.sharepoint.com/sites/sitename" -DaysBack 30
+
+This example searches the past 30 days for deletion events of the specified SharePoint site and exports the results to a CSV file.
+
+.NOTES
+Author: Wes Blackwell
+Last Updated: 5/17/2024
+Version: 1.0
+Required Modules: ExchangeOnlineManagement
+Prerequisites: Administrative rights are required to connect to Microsoft 365 Compliance Center, and the appropriate permissions to access the audit log.
+
+#>
+
 param(
     [Parameter(Mandatory=$true)]
     [string]$SiteUrl,
